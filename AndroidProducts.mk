a4cb9c052430a39b5b71e18e4df71e433faef20d
alternoegraha <noegrahachan@gmail.com>
2023-09-20 12:42:55 +0000

    fog: DroidX-UI bringup!
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_fog.mk
+    $(LOCAL_DIR)/droidx_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_fog-user \
-    aosp_fog-userdebug \
-    aosp_fog-eng
+    droidx_fog-user \
+    droidx_fog-userdebug \
+    droidx_fog-eng
