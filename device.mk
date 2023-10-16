df90efffde0201bcea4f1c79572fe5a69b22c0db
alternoegraha <noegrahachan@gmail.com>
2023-10-16 12:02:35 +0000

    Revert "fog: Add Support for CLO's Boost framework"
    This reverts commit 8907b09b4b83c9087607caec2e9ea8fabc3ad164 and 8a5dc8e6eade885652fb61df77f831bf4bf7f40f.

--- device.mk
+++ device.mk
@@ -363,14 +363,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0 \
-    android.hardware.thermal@2.0.vendor \
-    libpsi.vendor \
-    libtflite
-
-PRODUCT_BOOT_JARS += \
-    QPerformance \
-    UxPerformance
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
